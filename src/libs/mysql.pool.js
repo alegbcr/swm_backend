1fe77de5c2f9e4315f9b416fb55cbf804b005b18
Alexander Gonzalez <alexgbcr@proton.me>
2025-09-27 00:29:25 -0600

    [UPDATE] - Changed files in libs file

---
+++ src/libs/mysql.pool.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2/promise');
+const { config } = require('../../config/config');
+
+let pool;
+
+if (config.isProd) {
+  URI = mysql.createPool(config.dbUrl); // URL de MySQL en prod
+} else {
+  pool = mysql.createPool({
+    host: config.dbHost,
+    user: config.dbUser,
+    password: config.dbPassword,
+    database: config.dbName,
+    port: config.dbPort,
+  });
+}
+
+module.exports = pool;
--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -2,14 +2,17 @@ const { Sequelize } = require('sequelize');
 const { config } = require('../../config/config');
 const setupModels = require('../db/models/index');
 
-const sequelize = new Sequelize(config.dbUrl, {
-  dialect: 'mysql',
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
-});
+const sequelize = config.isProd
+  ? // Producción → MySQL real
+    (sequelize = new Sequelize(config.url, {
+      dialect: 'mysql',
+      dialectOptions: {
+        ssl: { rejectUnauthorized: false },
+      },
+      logging: false,
+    }))
+  : // Desarrollo → DB en memoria para no depender de MySQL real
+    (sequelize = new Sequelize('sqlite::memory:', { logging: false }));
 
 setupModels(sequelize);
 
